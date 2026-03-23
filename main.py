f73f4b9b47192d8132d509f3e63149a7a85fb6ba
Jayda Kiongo <265140237+JaydaKiongo68@users.noreply.github.com>
2026-03-23 18:54:13 -0400

    imported game library and set constants

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from tkinter import *
+import random
+
+#gamesettings
+GAME_WIDTH = 700
+GAME_HIGHT = 700
+SPEED = 50
+BODY_PARTS = 3
+SNAKE_COLOR = '#00FF00'
+FODD_COLOR = '#FF0000'
+BACKROUND_COLOR = '#000000'
