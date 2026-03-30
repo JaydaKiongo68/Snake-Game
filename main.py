01936dbc465b731f1c60c3f01a9aa2bb64f5f98a
Jayda Kiongo <265140237+JaydaKiongo68@users.noreply.github.com>
2026-03-30 18:45:10 -0400

    built out state class

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ BODY_PARTS = 3
 SNAKE_COLOR = '#00FF00'
 FODD_COLOR = '#FF0000'
 BACKROUND_COLOR = '#000000'
+
+#classes
+class Snake:
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0,0])
+
+        for x,y in self.coordinates:
+            square = canvas.create_rectangle(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill = SNAKE_COLOR,tag = 'snake')
+            self.squares.append(square)
